384ef9a6f97e7b79ecb486fb78cb5ebc023724c3
The-Average-Coder <117310223+The-Average-Coder@users.noreply.github.com>
2022-12-04 18:31:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import math
 pygame.init()
 
 ctypes.windll.user32.SetProcessDPIAware()
-screen = pygame.display.set_mode((1800, 1000))
+
+WIDTH = 1800
+HEIGHT = 1000
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Sim")
 
 ORANGE = (255, 200, 0)
@@ -15,7 +19,7 @@ GREEN = (100, 255, 100)
 YELLOW = (255, 255, 0)
 MAGENTA = (255, 150, 255)
 VIOLET = (200, 0, 200)
-
+WHITE = (255, 255, 255)
 
 class CelestialObject:
     def __init__(self, colour, radius, mass, xPos, yPos, initialXForce, initialYForce):
@@ -50,65 +54,117 @@ celestialObjects = [
     CelestialObject(VIOLET, 18, 4_300_000_000, 1800, 0, 0, 26_000_000_000),
     CelestialObject(GREEN, 20, 4_500_000_000, 1500, 0, 0, 30_000_000_000),
     CelestialObject(YELLOW, 25, 5_000_000, 1200, 0, 0, 40_000_000),
-    CelestialObject(MAGENTA, 40, 12_000_000_000, 700, 0, 0, 100_000_000_000)
+    CelestialObject(MAGENTA, 40, 10_000_000_000, 700, 0, 0, 100_000_000_000),
+
+    CelestialObject(ORANGE, 100, 1_500_000_000_000_000, 5000, 0, 0, 0),
+    CelestialObject(AQUA, 30, 6_000_000_000, 6000, 0, 0, 50_000_000_000),
+    CelestialObject(RED, 15, 4_000_000_000, 7000, 0, 0, 23_000_000_000),
+    CelestialObject(VIOLET, 18, 4_300_000_000, 6800, 0, 0, 26_000_000_000),
+    CelestialObject(GREEN, 20, 4_500_000_000, 6500, 0, 0, 30_000_000_000),
+    CelestialObject(YELLOW, 25, 5_000_000, 6200, 0, 0, 40_000_000),
+    CelestialObject(MAGENTA, 40, 10_000_000_000, 5700, 0, 0, 100_000_000_000),
+
+    CelestialObject(ORANGE, 100, 1_500_000_000_000_000, -5000, 0, 0, 0),
+    CelestialObject(AQUA, 30, 6_000_000_000, -4000, 0, 0, 50_000_000_000),
+    CelestialObject(RED, 15, 4_000_000_000, -3000, 0, 0, 23_000_000_000),
+    CelestialObject(VIOLET, 18, 4_300_000_000, -3200, 0, 0, 26_000_000_000),
+    CelestialObject(GREEN, 20, 4_500_000_000, -3500, 0, 0, 30_000_000_000),
+    CelestialObject(YELLOW, 25, 5_000_000, -3800, 0, 0, 40_000_000),
+    CelestialObject(MAGENTA, 40, 10_000_000_000, -4300, 0, 0, 100_000_000_000),
+
+
+    CelestialObject(ORANGE, 100, 1_500_000_000_000_000, 0, 5000, 0, 0),
+    CelestialObject(AQUA, 30, 6_000_000_000, 1000, 5000, 0, 50_000_000_000),
+    CelestialObject(RED, 15, 4_000_000_000, 2000, 5000, 0, 23_000_000_000),
+    CelestialObject(VIOLET, 18, 4_300_000_000, 1800, 5000, 0, 26_000_000_000),
+    CelestialObject(GREEN, 20, 4_500_000_000, 1500, 5000, 0, 30_000_000_000),
+    CelestialObject(YELLOW, 25, 5_000_000, 1200, 5000, 0, 40_000_000),
+    CelestialObject(MAGENTA, 40, 10_000_000_000, 700, 5000, 0, 100_000_000_000),
+
+    CelestialObject(ORANGE, 100, 1_500_000_000_000_000, 5000, 5000, 0, 0),
+    CelestialObject(AQUA, 30, 6_000_000_000, 6000, 5000, 0, 50_000_000_000),
+    CelestialObject(RED, 15, 4_000_000_000, 7000, 5000, 0, 23_000_000_000),
+    CelestialObject(VIOLET, 18, 4_300_000_000, 6800, 5000, 0, 26_000_000_000),
+    CelestialObject(GREEN, 20, 4_500_000_000, 6500, 5000, 0, 30_000_000_000),
+    CelestialObject(YELLOW, 25, 5_000_000, 6200, 5000, 0, 40_000_000),
+    CelestialObject(MAGENTA, 40, 10_000_000_000, 5700, 5000, 0, 100_000_000_000),
+
+    CelestialObject(ORANGE, 100, 1_500_000_000_000_000, -5000, 5000, 0, 0),
+    CelestialObject(AQUA, 30, 6_000_000_000, -4000, 5000, 0, 50_000_000_000),
+    CelestialObject(RED, 15, 4_000_000_000, -3000, 5000, 0, 23_000_000_000),
+    CelestialObject(VIOLET, 18, 4_300_000_000, -3200, 5000, 0, 26_000_000_000),
+    CelestialObject(GREEN, 20, 4_500_000_000, -3500, 5000, 0, 30_000_000_000),
+    CelestialObject(YELLOW, 25, 5_000_000, -3800, 5000, 0, 40_000_000),
+    CelestialObject(MAGENTA, 40, 10_000_000_000, -4300, 5000, 0, 100_000_000_000),
+
+
+    CelestialObject(ORANGE, 100, 1_500_000_000_000_000, 0, -5000, 0, 0),
+    CelestialObject(AQUA, 30, 6_000_000_000, 1000, -5000, 0, 50_000_000_000),
+    CelestialObject(RED, 15, 4_000_000_000, 2000, -5000, 0, 23_000_000_000),
+    CelestialObject(VIOLET, 18, 4_300_000_000, 1800, -5000, 0, 26_000_000_000),
+    CelestialObject(GREEN, 20, 4_500_000_000, 1500, -5000, 0, 30_000_000_000),
+    CelestialObject(YELLOW, 25, 5_000_000, 1200, -5000, 0, 40_000_000),
+    CelestialObject(MAGENTA, 40, 10_000_000_000, 700, -5000, 0, 100_000_000_000),
+
+    CelestialObject(ORANGE, 100, 1_500_000_000_000_000, 5000, -5000, 0, 0),
+    CelestialObject(AQUA, 30, 6_000_000_000, 6000, -5000, 0, 50_000_000_000),
+    CelestialObject(RED, 15, 4_000_000_000, 7000, -5000, 0, 23_000_000_000),
+    CelestialObject(VIOLET, 18, 4_300_000_000, 6800, -5000, 0, 26_000_000_000),
+    CelestialObject(GREEN, 20, 4_500_000_000, 6500, -5000, 0, 30_000_000_000),
+    CelestialObject(YELLOW, 25, 5_000_000, 6200, -5000, 0, 40_000_000),
+    CelestialObject(MAGENTA, 40, 10_000_000_000, 5700, -5000, 0, 100_000_000_000),
+
+    CelestialObject(ORANGE, 100, 1_500_000_000_000_000, -5000, -5000, 0, 0),
+    CelestialObject(AQUA, 30, 6_000_000_000, -4000, -5000, 0, 50_000_000_000),
+    CelestialObject(RED, 15, 4_000_000_000, -3000, -5000, 0, 23_000_000_000),
+    CelestialObject(VIOLET, 18, 4_300_000_000, -3200, -5000, 0, 26_000_000_000),
+    CelestialObject(GREEN, 20, 4_500_000_000, -3500, -5000, 0, 30_000_000_000),
+    CelestialObject(YELLOW, 25, 5_000_000, -3800, -5000, 0, 40_000_000),
+    CelestialObject(MAGENTA, 40, 10_000_000_000, -4300, -5000, 0, 100_000_000_000),
 ]
 
-inverseScaleFactor = 4
+inverseScaleFactor = 10
 
 relativeCelestialObject = celestialObjects[0]
 
 cameraPos = pygame.Vector2(0, 0)
-centre = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+centre = pygame.Vector2(WIDTH / 2, HEIGHT / 2)
 
 running = True
 while running:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             running = False
-        if i.type == pygame.KEYDOWN:
+        elif i.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            clickedObjects = [j for j in celestialObjects if j.rect.collidepoint(pos)]
+            if len(clickedObjects) > 0:
+                relativeCelestialObject = clickedObjects[0]
+        elif i.type == pygame.KEYDOWN:
             if i.key == pygame.K_ESCAPE:
                 running = False
+            elif i.key == pygame.K_RIGHT:
+                relativeCelestialObject = celestialObjects[(celestialObjects.index(relativeCelestialObject) + 1) % len(celestialObjects)]
+            elif i.key == pygame.K_LEFT:
+                relativeCelestialObject = celestialObjects[(celestialObjects.index(relativeCelestialObject) - 1) % len(celestialObjects)]
 
     cameraPos = relativeCelestialObject.position
 
     for i in celestialObjects:
         for j in celestialObjects:
-            if i == j:
+            if i is j:
                 continue
-            distance = math.hypot(i.position.x-j.position.x,
-                                  i.position.y-j.position.y)
+            distance = math.hypot(i.position.x-j.position.x, i.position.y-j.position.y)
             force = 0.00000000006743015 * ((i.mass * j.mass) / distance**2)
-            radians = math.atan2(j.position.y - i.position.y,
-                                 j.position.x - i.position.x)
+            radians = math.atan2(j.position.y - i.position.y, j.position.x - i.position.x)
             i.applyForce(force, radians)
         i.simulate()
-    """
-        distance = math.hypot(star.position.x-planet.position.x,
-                              star.position.y-planet.position.y)
-        force = 0.00000000006743015 * ((star.mass * planet.mass) / distance**2)
 
-        starRadians = math.atan2(
-            planet.position.y - star.position.y, planet.position.x - star.position.x)
-        planetRadians = math.atan2(
-            star.position.y - planet.position.y, star.position.x - planet.position.x)
-
-        star.applyForce(force, starRadians)
-        planet.applyForce(force, planetRadians)
-
-        star.simulate()
-        planet.simulate()
-    """
     screen.fill((0, 0, 0))
 
     for i in celestialObjects:
-        pygame.draw.circle(screen, i.colour, ((i.position.x -
-                           cameraPos.x) / inverseScaleFactor + centre.x, (i.position.y - cameraPos.y) / inverseScaleFactor + centre.y), i.radius / inverseScaleFactor)
-    """
-    pygame.draw.circle(screen, (255, 0, 0),
-                       star.position - cameraPos + centre, star.radius)
-    pygame.draw.circle(screen, (0, 0, 255),
-                       planet.position - cameraPos + centre, planet.radius)
-    """
+        i.rect = pygame.draw.circle(screen, i.colour, ((i.position.x - cameraPos.x) / inverseScaleFactor + centre.x, (i.position.y - cameraPos.y) / inverseScaleFactor + centre.y), i.radius / inverseScaleFactor)
+    
     pygame.display.flip()
 
     pygame.time.Clock().tick(120)
